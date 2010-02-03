39cb3eed7566ac0a7db6b579c958b66a126b788c
Maciej Piechotka <uzytkownik2@gmail.com>
2010-02-03 20:21:35 +0000

    Sharing internals between modules

---
+++ src-unix/Network/Interface.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{- |
+Module      : $Header$
+Description : Basic operation on iterfaces
+Copyright   : (c) Maciej Piechotka
+License     : BSD3
+
+Stability   : none
+Portability : POSIX
+
+Basic operations on interface
+-}
+module Network.Interface
+  (
+    Interface(..),
+    bringUp,
+    bringDown,
+    setMTU,
+    getMTU,
+    setIPv4,
+    getIPv4,
+    setIPv4Mask,
+    getIPv4Mask,
+  )
+where
+import Network.Interface.Internal
